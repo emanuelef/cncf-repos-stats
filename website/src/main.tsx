6c2b850a53c2e51e3ad3de61145120e20887734a
Emanuele Fumagalli <emanuelef@gmail.com>
2023-09-14 08:22:44 +0100

    fix deprecations

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -2,17 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { ProSidebarProvider } from "react-pro-sidebar";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 import { BrowserRouter } from "react-router-dom";
 root.render(
   <React.StrictMode>
     <div>
-      <ProSidebarProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ProSidebarProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </div>
   </React.StrictMode>
 );
