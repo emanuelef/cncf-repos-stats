b499c449bfd1ac566c552e37c9ca8d0c423fbc14
Emanuele Fumagalli <emanuelef@gmail.com>
2023-10-26 14:11:35 +0100

    Added Github corner

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -4,10 +4,16 @@ import "./index.css";
 import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 import { HashRouter } from "react-router-dom";
+import GitHubCorners from "@uiw/react-github-corners";
+
 root.render(
   <React.StrictMode>
     <div>
       <HashRouter>
+        <GitHubCorners
+          position="right"
+          href="https://github.com/emanuelef/cncf-repos-stats"
+        />
         <App />
       </HashRouter>
     </div>
